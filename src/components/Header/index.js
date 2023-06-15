cb2a006e1997ed8b9cce4f42a2e06aaabbac72a5
milosmilutinovic96 <milosmilutinoviclearn@gmail.com>
2023-06-15 23:38:47 +0200

    project setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import styles from './index.module.css';
+
+const Header = () => {
+    return (
+        <header>
+            <span>Regu servis</span>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,12 @@
+import styles from './index.module.css';
+
+const HeroSection = () => {
+    return (
+        <section>
+            <h1>Just paint</h1>
+            <p>Paint your home in colors that make you feel good.</p>
+        </section>
+    )
+}
+
+export default HeroSection;
